50b6dfb1437d36a5d59b3441b9b01e68b8cae104
Pedro Henrique Panstein <phfarof@gmail.com>
2024-09-04 16:51:08 -0300

    html quase finalizado, e implementação de sidebar com js e css 16:50 - 04/09/2024

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ AOS.init({
   duration: 2000,
   once: true,
 });
+
+const menu1 = document.querySelector(".menu-line1");
+const menu2 = document.querySelector(".menu-line2");
+const menu3 = document.querySelector(".menu-line3");
+const menuContent = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar")
+const background = document.querySelector(".active-sidebar")
+
+menuContent.addEventListener("click", () => {
+  menu1.classList.toggle("effect1");
+  menu2.classList.toggle("effect2");
+  menu3.classList.toggle("effect3");
+  sidebar.classList.toggle("hidden")
+  background.classList.toggle("hidden")
+  console.log("a");
+});
